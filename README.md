5da7862b0e77df77409c2f5bf5c8041796b95159
Collin Henderson <hendo13@me.com>
2013-12-01 12:53:52 -0500

    Migration fixes

--- README.md
+++ README.md
@@ -17,9 +17,11 @@ Simple deploys for your projects.
 
 ## Usage
 
-The easiest way to use Slipspace is to copy the function from the `slipspace.sh` file into your bash profile and reload it it (`source ~/.profile`). Next create a configuration file called `slipspace.json` somewhere in your project (the root is usually the best place). Have a look at the included config file to see how it should be structured.
+The easiest way to use Slipspace is to copy the function from the `slipspace.sh` file into your bash profile and reload it (`source ~/.profile`). Next create a configuration file called `slipspace.json` somewhere in your project (the root is usually the best place). Have a look at the included config file to see how it should be structured.
 
 Now you can `cd` into the directory where your config file lives and call `slipspace deploy your_environment`. The environment name should match the Git branch you use for a specific environment.
+
+Need to run migrations? Add a `--migrate` flag at the end.
 
 ## Features
 
@@ -30,5 +32,4 @@ Now you can `cd` into the directory where your config file lives and call `slips
 ## Todo
 
 - Better error handling
-- Finish migration stuff
 
--- slipspace.sh
+++ slipspace.sh
@@ -1,6 +1,6 @@
 function slipspace() {
-  if [ $# -ne 2 ]; then
-    echo "Usage: slipspace deploy [environment]"
+  if [ ! $# -ge 2 ]; then
+    echo "Usage: slipspace deploy [environment] [options]"
   else
     if [ $1 = "deploy" ]; then
       if [ ! -f slipspace.json ]; then
@@ -15,15 +15,15 @@ function slipspace() {
         path="${path//\"}"
         after=`cat slipspace.json | jq -c -r ".environments.$env.after[] + \";\" "`
         migrate=`cat slipspace.json | jq ".environments.$env.migrate // null"`
-        if [ "$migrate" = "null" ]; then
-          ssh -t $user@$host "echo 'Deploying to ${env}...'; cd ${path}; echo 'Fetching source...'; git reset --hard; git checkout ${env}; git pull origin ${env}; echo 'Running post-deploy tasks...'; ${after}exit; bash"
-        else
+        if [ ! "$migrate" = "null" ] && [ ! -z $3 ] && [ $3 = "--migrate" ]; then
           ssh -t $user@$host "echo 'Deploying to ${env}...'; cd ${path}; echo 'Fetching source...'; git reset --hard; git checkout ${env}; git pull origin ${env}; echo 'Running post-deploy tasks...'; ${after}echo 'Running migrations...'; ${migrate}; exit; bash"
+        else
+          ssh -t $user@$host "echo 'Deploying to ${env}...'; cd ${path}; echo 'Fetching source...'; git reset --hard; git checkout ${env}; git pull origin ${env}; echo 'Running post-deploy tasks...'; ${after}exit; bash"
         fi
         echo "Deployment to ${env} has finished."
       fi  
     else
-      echo "Invalid command. Usage: slipspace deploy [environment]"
+      echo "Invalid command. Usage: slipspace deploy [environment] [options]"
     fi  
   fi  
 }
